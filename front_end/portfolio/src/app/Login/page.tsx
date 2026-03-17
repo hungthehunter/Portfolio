85acfea8dd3065776e56454be0d44b86fdc93e17
hungthehunter <133243197+hungthehunter@users.noreply.github.com>
2026-03-17 00:26:14 -0700

    feat: add home page
    - add login component, page
    - add register component, page
    - add home component, page

---
+++ front_end/portfolio/src/app/Login/page.tsx
@@ -0,0 +1,7 @@
+import {Login as LoginComponent} from "@/src/components/Login/Login"
+
+export default function Login(){
+    return (
+        <LoginComponent/>
+    )
+}
---
+++ front_end/portfolio/src/components/Home/Home.tsx
@@ -0,0 +1,5 @@
+export function Home(){
+    return(
+        <div>Hello world Home</div>
+    )
+}
